bdf8ba736e9a6135e393f8afa7106d0cf6a273ae
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-04-07 07:11:09 +0800

    refactor: move kelas page

---
+++ src/pages/modul/akademik/referensi/kelas/kelas-page.tsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import {AddButton} from "@/components/ui/button.tsx";
+import CustomHeader from "@/components/shared/custom-header.tsx";
+import AkademikLayout from "@/components/layout/akademik-layout.tsx";
+import TahunAjaranTable from "@/pages/modul/akademik/referensi/tahun-ajaran/components/tahun-ajaran-table.tsx";
+import TahunAjaranModal from "@/pages/modul/akademik/referensi/tahun-ajaran/components/tahun-ajaran-modal.tsx";
+import useGroupModal from "@/hooks/useGroupModal.tsx";
+import useParams from "@/hooks/useParams.tsx";
+import TahunAjaranEntity from "@/pages/modul/akademik/referensi/tahun-ajaran/data/tahun-ajaran.entity.ts";
+
+
+const KelasPage: React.FC = () => {
+    const {groupModal, handleGroupModal} = useGroupModal({
+        modal: false
+    })
+    const {params,handleParamsChange} = useParams({})
+    const [selectedData, setSelectedData] = useState<TahunAjaranEntity>()
+
+    return (<AkademikLayout>
+            <section className={"px-12 py-4"}>
+                <CustomHeader
+                    title={"Data Kelas"}
+                    handleSearch={(value) => handleParamsChange("search",value)}
+                />
+                <div className={"flex justify-end  py-2 gap-1"}>
+                    <AddButton onClick={() => handleGroupModal('modal', true)}/>
+                </div>
+                <TahunAjaranTable
+                    setSelectedData={setSelectedData}
+                    handleGroupModal={handleGroupModal}
+                    params={params}
+                />
+            </section>
+            <TahunAjaranModal
+                isOpen={groupModal.modal}
+                handleGroupModal={handleGroupModal}
+                selectedData={selectedData}
+                setSelectedData={setSelectedData}
+            />
+        </AkademikLayout>
+
+    );
+};
+
+export default KelasPage;
--- src/routes/akademik/referensi/kelas.tsx
+++ src/routes/akademik/referensi/kelas.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import KelasPage from "@/pages/modul/akademik/referensi/kelas/kelas-page.tsx";
 
 export const Route = createFileRoute('/akademik/referensi/kelas')({
-  component: () => <div>Hello /akademik/referensi/kelas!</div>
+  component: () => <KelasPage />
 })
