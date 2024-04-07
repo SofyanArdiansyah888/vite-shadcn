f43b9a14f3553edc04bace3c277297c59fe0cb35
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-04-08 05:56:15 +0800

    style: add colspan

--- src/components/shared/modal/detail-modal.tsx
+++ src/components/shared/modal/detail-modal.tsx
@@ -7,37 +7,38 @@ interface IDetailModal {
     title: string,
     isOpen: boolean,
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
-    details: { key: string, value: string }[]
+    details: { key: string, value: string,colspan?: string }[],
+    column?: string
 }
 
 export default function DetailModal({
-                                           title,
-                                           isOpen,
-                                           setIsOpen,
-                                           details
-                                       }: IDetailModal) {
+                                        title,
+                                        isOpen,
+                                        setIsOpen,
+                                        details,
+                                        column = "grid-cols-2"
+                                    }: IDetailModal) {
 
     return <Modal
         title={title}
         open={isOpen}
         onCancel={() => setIsOpen(false)}
         destroyOnClose
-        className={""}
-        footer={[  <Button key="back" onClick={() => setIsOpen(false)}>
+        footer={[<Button key="back" onClick={() => setIsOpen(false)}>
             Tutup
         </Button>]}
     >
-        <Separator className={"mb-8"} />
-        <section className={"grid grid-cols-2 gap-x-6 gap-y-4"}>
+        <Separator className={"mb-8"}/>
+        <section className={`grid ${column} gap-x-6 gap-y-4`}>
             {
                 details.map((item, index) => {
-                    return <div key={index}>
+                    return <div key={index} className={`${item.colspan ?? "col-span-1"}`}>
                         <h6 className={"font-medium"}>{item.key}</h6>
                         <p className={"font-light"}>{item.value}</p>
                     </div>
                 })
             }
         </section>
-        <Separator className={"mt-8"} />
+        <Separator className={"mt-8"}/>
     </Modal>
 }
