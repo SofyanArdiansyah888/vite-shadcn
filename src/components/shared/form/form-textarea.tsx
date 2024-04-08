dc2a69dc955a5cdaf716af07ccdf6bc6cd2d756a
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-04-08 08:40:14 +0800

    feat: add new component

---
+++ src/components/shared/form/form-textarea.tsx
@@ -0,0 +1,32 @@
+import {Form, Input} from "antd";
+import {RuleObject, RuleRender} from "rc-field-form/lib/interface";
+
+const { TextArea } = Input;
+export interface IFormSelectValue {
+    value: string,
+    label: string,
+    disabled?: boolean
+}
+
+interface IFormSelect {
+    name: string,
+    label: string,
+    value?: string,
+    onChange?: any,
+    placeholder?: string,
+    rules?: RuleObject[] | RuleRender[]
+}
+
+export default function FormTextarea({
+                                      name,
+                                      label,
+                                      onChange,
+                                      placeholder,
+                                      rules,
+                                      value
+                                  }: IFormSelect) {
+    return <Form.Item name={name} label={label} rules={rules}>
+        <TextArea rows={4} onChange={onChange} placeholder={placeholder} value={value}/>
+    </Form.Item>
+
+}
